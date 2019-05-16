c1d60e87244311200a5d33ca3a906094849bc811
Krista Brehm <kristabrehm1@gmail.com>
2019-05-16 17:56:22 -0400

    added a little

--- mern-auth/client/src/components/Footer.js
+++ mern-auth/client/src/components/Footer.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return (
+            <footer>This is a footer</footer>
         );
     }
 }
--- mern-auth/client/src/components/Navbar.js
+++ mern-auth/client/src/components/Navbar.js
@@ -4,6 +4,9 @@ import React, { Component } from 'react';
 class NavBar extends Component {
     render() {
         return (
+            <nav>
+                This is a nav bar
+            </nav>
         );
     }
 }
