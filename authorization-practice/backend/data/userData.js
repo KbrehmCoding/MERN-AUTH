f3a8c172f60093f1384389e4f4998bab45126f34
Krista Brehm <kristabrehm1@gmail.com>
2019-05-22 15:30:34 -0400

    setting up sample user data and backend folders

---
+++ authorization-practice/backend/data/userData.js
@@ -0,0 +1,37 @@
+[
+    {
+        "username": "person1",
+        "userpassword": "apple",
+        "useremail": "example1@gmail.com"
+    },
+    {
+        "username": "person2",
+        "userpassword": "orange",
+        "useremail": "example2@gmail.com"
+    },
+    {
+        "username": "person3",
+        "userpassword": "banana",
+        "useremail": "example3@gmail.com"
+    },
+    {
+        "username": "person4",
+        "userpassword": "lemon",
+        "useremail": "example4@gmail.com"
+    },
+    {
+        "username": "person5",
+        "userpassword": "pineapple",
+        "useremail": "example5@gmail.com"
+    },
+    {
+        "username": "person6",
+        "userpassword": "grape",
+        "useremail": "example6@gmail.com"
+    },
+    {
+        "username": "Krista Brehm",
+        "userpassword": "strawberry",
+        "useremail": "example@gmail.com"
+    }
+]
